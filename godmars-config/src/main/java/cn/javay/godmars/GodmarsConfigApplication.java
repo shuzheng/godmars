17da6603829e13c600998ba467446904a58a55b6
shuzheng <469741414@qq.com>
2017-06-30 23:45:26 +0800

    注册配置中心

--- godmars-config/src/main/java/cn/javay/godmars/GodmarsConfigApplication.java
+++ godmars-config/src/main/java/cn/javay/godmars/GodmarsConfigApplication.java
@@ -2,12 +2,12 @@ package cn.javay.godmars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class GodmarsConfigApplication {
 
 	public static void main(String[] args) {
