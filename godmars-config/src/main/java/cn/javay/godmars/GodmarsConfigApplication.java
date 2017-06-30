9f21b1de6129cde12b70d2c5dcf0ea168c265649
shuzheng <469741414@qq.com>
2017-06-30 23:37:30 +0800

    add config

--- godmars-config/src/main/java/cn/javay/godmars/GodmarsConfigApplication.java
+++ godmars-config/src/main/java/cn/javay/godmars/GodmarsConfigApplication.java
@@ -1,26 +1,14 @@
 package cn.javay.godmars;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
-@RestController
+@EnableConfigServer
+@EnableEurekaClient
 public class GodmarsConfigApplication {
-
-	@Value("${config.name}")
-	String name = "World";
-
-	@RequestMapping("/")
-	public String home() {
-		return "Hello " + name;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GodmarsConfigApplication.class, args);
