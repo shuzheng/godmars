76ea4359bd516f78181bf2a149ef8fe8f9a43bc0
shuzheng <469741414@qq.com>
2017-07-01 13:11:49 +0800

    使用spring cloud config

--- godmars-web/src/main/java/cn/javay/godmars/GodmarsWebApplication.java
+++ godmars-web/src/main/java/cn/javay/godmars/GodmarsWebApplication.java
@@ -2,13 +2,14 @@ package cn.javay.godmars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
 @EnableHystrixDashboard
--- godmars-web/src/main/java/cn/javay/godmars/controller/HelloControler.java
+++ godmars-web/src/main/java/cn/javay/godmars/controller/HelloControler.java
@@ -2,6 +2,7 @@ package cn.javay.godmars.controller;
 
 import cn.javay.godmars.service.FeignService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,17 @@ public class HelloControler {
     @Autowired
     FeignService feignService;
 
+    @Value("${from}")
+    String from;
+
     @RequestMapping(value = "/hi")
     public String hi(@RequestParam String name){
         return feignService.hi(name);
+    }
+
+    @RequestMapping(value = "/config")
+    public String config(){
+        return from + "<form action='http://localhost:50001/bus/refresh?destination=customers:**' method='post'><input type='submit'/></form>";
     }
 
 }
