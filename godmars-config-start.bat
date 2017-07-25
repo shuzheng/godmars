c481b1f1636384d90b1e17e0a1e724b6497c397b
2423 <234234234>
2017-07-25 16:06:32 +0800

    update

--- godmars-config-start.bat
+++ godmars-config-start.bat
@@ -1,3 +1,3 @@
 @echo off
 start java -jar godmars-config/target/godmars-config-0.0.1-SNAPSHOT.jar --server.port=20001
-rem start java -jar godmars-config/target/godmars-config-0.0.1-SNAPSHOT.jar --server.port=20002
+start java -jar godmars-config/target/godmars-config-0.0.1-SNAPSHOT.jar --server.port=20002
